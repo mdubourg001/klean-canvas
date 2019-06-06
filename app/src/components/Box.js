03c11ba8d3a308056cecfa5cfe412b6a3756b3c0
mdubourg <mdubourg@akema.fr>
2019-06-06 13:49:39 +0200

    feature: auto-scaling of textareas and styling on focus

--- app/src/components/Box.js
+++ app/src/components/Box.js
@@ -1,14 +1,67 @@
-import React from "react";
-import BoxInputList from "./BoxInputList";
+import React, { useState } from "react";
 
-const Box = ({ title, subtitle, cssClasses }) => {
+const DEFAULT_TEXTAREA_FONT_SIZE = 13;
+
+const Box = ({ title, subtitle, cssClasses, tabIndex }) => {
+  const [focused, setFocused] = useState(false);
+  const [textAreaFontSize, setTextAreaFontSize] = useState(
+    DEFAULT_TEXTAREA_FONT_SIZE
+  );
+  const [diffOuterScrollHeight, setDiffOuterScrollHeight] = useState(0);
+
+  /* Scaling textarea font-size based on its scrollHeight 
+     The goal is to always have the whole textarea content visible */
+  const onTextareaInput = e => {
+    e.target.style.height = e.target.scrollHeight + "px";
+    const styleHeight = parseInt(e.target.style.height.replace("px", ""));
+    if (e.target.offsetHeight < styleHeight) {
+      if (styleHeight - e.target.offsetHeight > diffOuterScrollHeight) {
+        setDiffOuterScrollHeight(styleHeight - e.target.offsetHeight);
+        setTextAreaFontSize(textAreaFontSize - 1);
+      }
+    } else if (textAreaFontSize < DEFAULT_TEXTAREA_FONT_SIZE) {
+      setDiffOuterScrollHeight(0);
+    }
+  };
+
   return (
     <div
-      className={`w-full h-full px-4 py-3 border border-gray-200 ${cssClasses}`}
+      className={`w-full h-full flex flex-col px-4 py-3 border border-gray-200 ${cssClasses}`}
+      style={
+        focused
+          ? {
+              backgroundColor: "#edf2f7",
+              borderColor: "transparent",
+              boxShadow: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+              transition: "transform 0.3s ease"
+            }
+          : {
+              boxShadow: "none",
+              transition: "box-shadow 0.3s ease"
+            }
+      }
     >
-      <h3 className="font-emphasized uppercase">{title}</h3>
-      <h4 className="text-xs text-gray-700 mb-2">{subtitle}</h4>
-      <BoxInputList />
+      <div>
+        <h3 className="text-xs font-emphasized uppercase">{title}</h3>
+        <h4 className="text-xs text-gray-700 mb-2">{subtitle}</h4>
+        <hr className="my-1 border-t border-gray-200" />
+      </div>
+
+      <textarea
+        style={{
+          resize: "none",
+          maxHeight: "100%",
+          fontSize: `${textAreaFontSize}px`,
+          backgroundColor: "transparent"
+        }}
+        tabIndex={tabIndex}
+        spellCheck="false"
+        autoCorrect="false"
+        className="w-full h-full focus:outline-none"
+        onInput={onTextareaInput}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      />
     </div>
   );
 };
--- app/src/components/LeanCanvas.js
+++ app/src/components/LeanCanvas.js
@@ -10,7 +10,8 @@ const LeanCanvas = () => {
             <Box
               title="Problem"
               subtitle="What problems does your idea solves ?"
-              cssClasses="border-t-0 border-l-0 border-b-0"
+              cssClasses="border-t-0 border-l-0 border-b-0 rounded-tl-lg"
+              tabIndex={1}
             />
           </div>
           <div className="h-1/3">
@@ -18,6 +19,7 @@ const LeanCanvas = () => {
               cssClasses="border-l-0 border-t-0"
               title="Existing alternatives"
               subtitle="How are these problems solved today ?"
+              tabIndex={1}
             />
           </div>
         </div>
@@ -28,12 +30,14 @@ const LeanCanvas = () => {
               cssClasses="border-t-0"
               title="Solution"
               subtitle="What are your solutions for these problems ?"
+              tabIndex={4}
             />
           </div>
           <div className="h-1/2">
             <Box
               title="Key metrics"
               subtitle="How to tell that your business is going well ?"
+              tabIndex={8}
             />
           </div>
         </div>
@@ -44,6 +48,7 @@ const LeanCanvas = () => {
               cssClasses="border-t-0 border-b-0"
               title="Unique value proposition"
               subtitle="How are you different from your concurrents ?"
+              tabIndex={3}
             />
           </div>
           <div className="h-1/3">
@@ -51,6 +56,7 @@ const LeanCanvas = () => {
               cssClasses="border-t-0"
               title="High-level concept"
               subtitle="Your X for Y analogy"
+              tabIndex={3}
             />
           </div>
         </div>
@@ -61,12 +67,14 @@ const LeanCanvas = () => {
               cssClasses="border-t-0"
               title="Unfair advantage"
               subtitle="Something that can't be easily copied by concurrence"
+              tabIndex={9}
             />
           </div>
           <div className="h-1/2">
             <Box
               title="Channels"
               subtitle="What are your paths to customers ?"
+              tabIndex={5}
             />
           </div>
         </div>
@@ -74,9 +82,10 @@ const LeanCanvas = () => {
         <div className="w-1/5">
           <div className="h-2/3">
             <Box
-              cssClasses="border-t-0 border-r-0 border-b-0"
+              cssClasses="border-t-0 border-r-0 border-b-0 rounded-tr-lg"
               title="Customer segments"
               subtitle="List your target customers and users"
+              tabIndex={2}
             />
           </div>
           <div className="h-1/3">
@@ -84,6 +93,7 @@ const LeanCanvas = () => {
               cssClasses="border-r-0 border-t-0"
               title="Early adopters"
               subtitle="Characteristics of your ideal customer"
+              tabIndex={2}
             />
           </div>
         </div>
@@ -92,16 +102,18 @@ const LeanCanvas = () => {
       <div className="h-1/3 flex">
         <div className="w-1/2">
           <Box
-            cssClasses="border-b-0 border-l-0"
+            cssClasses="border-b-0 border-l-0 rounded-bl-lg"
             title="Costs"
             subtitle="List your fixed and variable costs"
+            tabIndex={7}
           />
         </div>
         <div className="w-1/2">
           <Box
-            cssClasses="border-b-0 border-r-0"
+            cssClasses="border-b-0 border-r-0 rounded-br-lg"
             title="Revenues"
             subtitle="List your sources of revenue"
+            tabIndex={6}
           />
         </div>
       </div>
